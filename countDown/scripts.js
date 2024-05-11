87962dc3a5bd0aab87abe4b8343de16a8231202c
Salwa-mansour <alqadasisal@gmail.com>
2024-05-11 17:40:59 +0300

    countDown timer is ready

--- countDown/scripts.js
+++ countDown/scripts.js
@@ -1,12 +1,17 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
-
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time');
 
 function timer(seconds){
+    // clear any existing timers
+    clearInterval(countdown);
     const now = Date.now(); //save the starting point
     const then = now + seconds * 1000 ;// save the end time *we save them just in case function has stoped accourding to browser performance treets. 
     // display for the firest time (befor interval runs)
-    displayTimeLeft(seconds)
+    displayTimeLeft(seconds);
+
+    displayEndTime(then);
     //change the timer inner text 
   countdown =  setInterval(()=>{
         const secondsLeft =Math.round((then -Date.now())/1000);
@@ -24,7 +29,33 @@ function timer(seconds){
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds/60);
     const remaiderSeconds = seconds % 60 ;
-    const display =`${minutes}:${remaiderSeconds}`;
+    const display =`${minutes}:${remaiderSeconds < 10 ?'0':''}${remaiderSeconds}`;
     timerDisplay.textContent = display;
-    console.log({minutes, remaiderSeconds});
+    document.title = display;
+    
 }
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const adjustedHour = hours >12 ? hours - 12 : hours;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Bck At ${adjustedHour}:${minutes < 10 ? '0':'' }${minutes} ${ hours > 12 ? 'PM':'AM'}`
+    
+}
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+  
+}
+
+
+buttons.forEach(button =>button.addEventListener('click',startTimer));
+document.customForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    console.log(mins)
+    this.reset();
+
+})
